a73fae4af5b5a4d7a4e295a5b2bc2a94106ce504
cyberflohr <wolfgang@cyberflohrs.de>
2013-07-06 15:14:17 +0200

    webview example enhanced

--- examples/webview/src/main/java/org/jshybugger/example/webview/MainActivity.java
+++ examples/webview/src/main/java/org/jshybugger/example/webview/MainActivity.java
@@ -1,11 +1,16 @@
 package org.jshybugger.example.webview;
 
+import org.jshybugger.DebugContentProvider;
 import org.jshybugger.DebugServiceClient;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.webkit.ConsoleMessage;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
 import android.webkit.WebChromeClient;
@@ -32,8 +37,10 @@ public class MainActivity extends Activity {
 		});
 		
 		WebSettings settings = webView.getSettings();
+		
 		settings.setJavaScriptEnabled(true);
 		settings.setAllowFileAccess(true);
+		
 		settings.setAppCacheEnabled(true);
 		settings.setDomStorageEnabled(true);
 		settings.setAllowContentAccess(true);
@@ -44,14 +51,23 @@ public class MainActivity extends Activity {
 		settings.setDatabasePath(databasePath); 
 		
 		// attach web view to debugging service 
-		DebugServiceClient.attachWebView(webView, this);
+		DebugServiceClient dbgClient = DebugServiceClient.attachWebView(webView, this);
 
-		// load html page via JsHybugger content provider 
-		// example for remote page loading	
+		// load remote html page via JsHybugger content provider 
 		//webView.loadUrl("content://jsHybugger.org/http://www.jsHybugger.org/angular-phonecat/app/index.html");
 
 		// example for local page loading	
 		webView.loadUrl("content://jsHybugger.org/file:///android_asset/www/index.html");
+
+		/* example for loadDataWithBaseURL
+		String resUri = dbgClient.processJSCode("function hello() {\nvar x=0;\nalert('hello world');\n}", null);
+
+		final String htmlPre = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"> <script src='" + dbgClient.getJsHybuggerURL() + "'></script> <script src='" + resUri + "'></script></head><body style='margin:0; pading:0; background-color: black;'>";  
+        final String htmlCode = "<button onclick='hello()'>hello world</button>";
+        final String htmlPost = "</body></html>";
+        
+        webView.loadDataWithBaseURL("null", htmlPre+htmlCode+htmlPost, "text/html", "UTF-8", null);
+        */
 	}
 
 	@Override
